cdf70eb7508c4264e7abcdffde78c9e2d8b8bed9
denisselet <96264844+denisselet@users.noreply.github.com>
2022-12-19 23:24:37 +0200

    fix: add exists

--- src/lib/removeFile.mjs
+++ src/lib/removeFile.mjs
@@ -2,11 +2,13 @@ import fs from "fs";
 import path from "path";
 
 export const removeFile = (file, currentDir) => {
-    const filePath = path.isAbsolute(file)
-        ? file
-        : path.join(currentDir, file);
-    fs.unlink(filePath, (err) => {
-        if (err) throw err;
-        console.log("File deleted!");
-    });
+    const filePath = path.isAbsolute(file) ? file : path.join(currentDir, file);
+    if (fs.existsSync(filePath)) {
+        fs.unlink(filePath, (err) => {
+            if (err) throw err;
+            console.log("File deleted!");
+        });
+    } else {
+        console.log("Operation failed");
+    }
 };
